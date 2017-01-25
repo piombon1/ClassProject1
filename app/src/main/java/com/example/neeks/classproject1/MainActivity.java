6f825f8acd57739c65b5a797f1a9d1163ba5d8ba
piombon1 <piombon1@gmail.com>
2017-01-25 14:20:45 -0500

    Add button...
    In the mainactivity we created OnClick method with text and counter

--- app/src/main/java/com/example/neeks/classproject1/MainActivity.java
+++ app/src/main/java/com/example/neeks/classproject1/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.neeks.classproject1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mOnClick(View view) {
+        ( (Button)view).setText("Clicked " + (++count));
     }
 }
